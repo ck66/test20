552d2fe6c3c52a8373e4b6b741b5ceb81e109a9f
ck66 <ck66@users.noreply.github.com>
2026-06-28 14:47:19 +0800

    fix: 添加 Rect 导入

--- app/src/main/java/com/ck66/dusou/ui/search/PhotoSearchScreen.kt
+++ app/src/main/java/com/ck66/dusou/ui/search/PhotoSearchScreen.kt
@@ -72,6 +72,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.Stroke
